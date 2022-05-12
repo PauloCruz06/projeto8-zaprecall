69b57f969dada91e0b5b4c8b2dbfa5d53ca21fda
Paulo <pc5783346@gmail.com>
2022-05-12 02:38:00 -0300

    Criação do layout da tela inicial

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+export function Home(){
+    return(
+        <div className="home">
+            <img className="img-home" src="./images/logo2.png" />
+            <h1>ZapRecall</h1>
+            <button>Iniciar Recall!</button>
+        </div>
+    );
+}
